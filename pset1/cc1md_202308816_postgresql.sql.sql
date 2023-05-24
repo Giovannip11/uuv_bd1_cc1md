9fcf918d4e4f51bcd8e5fbbcc77363cfabd4f26d
Giovannip11 <125603962+Giovannip11@users.noreply.github.com>
2023-05-24 13:54:02 -0300

    Update cc1md_202308816_postgresql.sql.sql

--- pset1/cc1md_202308816_postgresql.sql.sql
+++ pset1/cc1md_202308816_postgresql.sql.sql
@@ -4,16 +4,16 @@ CREATE SCHEMA lojas;
 
 ALTER SCHEMA lojas OWNER TO Giovanni;
 CREATE TABLE lojas.produtos (
-                produto_id NUMERIC(38) NOT NULL,
-                nome VARCHAR(255) NOT NULL,
-                preco_unitario NUMERIC(10,2),
-                detalhes BYTEA,
-                imagem BYTEA,
-                imagem_mime_type VARCHAR(512),
-                imagem_arquivo VARCHAR(512),
-                imagem_charset VARCHAR(512),
-                imagem_ultima_atualizacao DATE,
-                CONSTRAINT produto_id PRIMARY KEY (produto_id)
+    produto_id NUMERIC(38) NOT NULL,
+    nome VARCHAR(255) NOT NULL,
+    preco_unitario NUMERIC(10,2),
+    detalhes BYTEA,
+    imagem BYTEA,
+    imagem_mime_type VARCHAR(512),
+    imagem_arquivo VARCHAR(512),
+    imagem_charset VARCHAR(512),
+    imagem_ultima_atualizacao DATE,
+    CONSTRAINT produto_id PRIMARY KEY (produto_id)
 );
 COMMENT ON TABLE lojas.produtos IS 'Tabela com as informações dos produtos, como nome e preço.';
 COMMENT ON COLUMN lojas.produtos.produto_id IS 'Coluna com o ID identificador dos produtos.';
